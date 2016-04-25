a51e25d58f84c6d2f62d979e1aec54310b05e4b5
uras@i.ua <Gigant2006>
2016-04-25 22:20:16 +0300

    Practice 1

--- practicum/src/FindMaxNumber.java
+++ practicum/src/FindMaxNumber.java
@@ -1,10 +1,10 @@
 public class FindMaxNumber {
     public int max(int[] input) {
-        int summa = 0;
+
         int max=input[0];
 
         for (int i : input) {
-            summa += i;
+
             if (max < i)
                 max = i;
 
