388a574c718bebd52a0897bb44f92a29ef39dff1
uras@i.ua <Gigant2006>
2016-04-25 21:53:52 +0300

    Practice 1

--- practicum/src/SumDigits.java
+++ practicum/src/SumDigits.java
@@ -6,14 +6,15 @@ public class SumDigits {
     public int sum(int number) {
         int result =0;
 
-        while (number!=0) {
-            if (number > 0) {
+       if (number <0){
+           number*=-1;
+       }
+        while (number>0) {
+
                 result = result + number % 10;
                 number /= 10;
-            }else {
-                result = result + (number % 10)*-1;
-                number /= 10;
-            }
+
+
         }
     return result;
     }
