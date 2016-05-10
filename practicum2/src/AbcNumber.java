645b5d2640589caa0491cd0c478f7871f4cf04bb
uras@i.ua <Gigant2006>
2016-05-10 22:57:56 +0300

    Practice 2

--- practicum2/src/AbcNumber.java
+++ practicum2/src/AbcNumber.java
@@ -5,6 +5,17 @@
 
 public class AbcNumber {
     public int convert(String num) {
+    char[] ch = num.toCharArray();
+        int res = 0;
+        for (int i=0;i<ch.length;i++)
+            res = res*10+(ch[i]-'a');
+        return res;
+    }
+}
+
+
+/*public class AbcNumber {
+    public int convert(String num) {
         int res = 0;
         int step = 0;
         String word = "abcdefghijklmnopqrstuvwxyz";
@@ -20,4 +31,26 @@ public class AbcNumber {
 
         return res;
     }
-}
+}*/
+/*
+public class AbcNumber {
+    public static void main(String[] args) {
+        System.out.println(convert("bcd"));
+    }
+
+    public static int convert(String num) {
+        int numLength = num.length();
+
+        StringBuilder resultFromMap = new StringBuilder();
+
+        Map<Character, Integer> symbolCorrespondenceTable = new TreeMap<>();
+        for (int i = 0; i < 10; i++) {
+            symbolCorrespondenceTable.put((char) ('a' + i), i);
+        }
+        for (int i = 0; i < numLength; i++) {
+            char ch = num.charAt(i);
+            resultFromMap.append(symbolCorrespondenceTable.get(ch));
+        }
+        return Integer.parseInt(resultFromMap.toString());
+    }
+}*/
