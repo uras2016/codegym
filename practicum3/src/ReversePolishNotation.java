fd9819b564cc9db522571dbd7f999c736aef3b3a
uras <uras@i.ua>
2016-05-19 22:50:57 +0300

    edit

--- practicum3/src/ReversePolishNotation.java
+++ practicum3/src/ReversePolishNotation.java
@@ -1,12 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 //Дано арифметичний вираз у вигляді Польського Інверсного запису.
 //        Необхідно обчислити значення виразу і повернути його.
 //
 //        Вираз складається лише з цілих чисел і операцій +, -, *, /. Гарантується, що результат також ціне число.
+
 public class ReversePolishNotation {
     public int evaluate(String expression) {
 
 
-        return 0;
+
+        String[] exp = expression.split(" ");
+
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        int result = 0;
+
+        for (String s : exp) {
+
+            if (!s.equals("+") && !s.equals("-") && !s.equals("*") && !s.equals("/")) {
+                deque.addFirst(s);
+                continue;
+            } else {
+                int firstNumber = Integer.parseInt(deque.removeFirst());
+                int secondNumber = Integer.parseInt(deque.removeFirst());
+                switch (s) {
+                    case "+":
+                        result = secondNumber + firstNumber;
+                        break;
+                    case "-":
+                        result=secondNumber-firstNumber;
+                        break;
+                    case"*":
+                        result = secondNumber*firstNumber;
+                        break;
+                    case "/":
+                        result = secondNumber/firstNumber;
+                        break;
+                }
+
+                deque.addFirst(String.valueOf(result));
+            }
+        }
+
+        return Integer.parseInt(deque.removeFirst());
     }
+
 }
