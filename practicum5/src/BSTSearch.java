df2ef30f40e7f85436fb5db0d3da4bd7438dd4dd
uras <uras@i.ua>
2016-05-26 22:36:22 +0300

    edit

--- practicum5/src/BSTSearch.java
+++ practicum5/src/BSTSearch.java
@@ -1,44 +1,35 @@
-
+//Знайти число в бінарному дереві пошуку і повернути true якщо воно присутнє, інакше повернути false.
 
 public class BSTSearch {
+
     public boolean exist(TreeNode root, int target) {
-        if (root == null)
-            return false;
-        if (root.getValue() == target) {
-            return true;
+
+        if (root == null) return false;
+
+        TreeNode currentNode = root;
+
+        while (true) {
+            if (target < currentNode.value) {
+                if (currentNode.left != null) {
+                    currentNode = currentNode.left;
+                } else {
+                    return false;
+                }
+            } else if (target > currentNode.value) {
+                if (currentNode.right != null) {
+                    currentNode = currentNode.right;
+                } else {
+                    return false;
+                }
+            } else if (target == currentNode.value) {
+                return true;
+            }
         }
-        if (root.getValue() > target) {
-            return exist(root.getLeft(), target);
-        } else
-            return exist(root.getRight(), target);
     }
 }
 
 class TreeNode {
     int value;
+
     TreeNode left, right;
-
-    public TreeNode(int value, TreeNode left, TreeNode right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public TreeNode getLeft() {
-        return left;
-    }
-
-    public TreeNode getRight() {
-        return right;
-    }
 }
-/*
-class TreeNode {
-  int value;
-  TreeNode left, right;
-}
-*/
